4d6cd117298a86bdadae3be2a07788a8f57acaa6
Chris Valleau <146878236+chris-cfa@users.noreply.github.com>
2025-03-07 10:37:26 -0500

    diagnostic plots from measles example

--- abctools/plot_utils.py
+++ abctools/plot_utils.py
@@ -1,4 +1,151 @@
+import os
 import matplotlib.pyplot as plt
+from abc_classes.py import SimulationBundle
+import polars as pl
+
+
+def duplicate_weights_dataframe(
+    sim_bundle: SimulationBundle,
+):
+    weights_df = pl.from_dict(
+        {
+            "simulation": list(sim_bundle.weights.keys()),
+            "weight": list(sim_bundle.weights.values()),
+        }
+    )
+
+    sim_bundle.accept_results = (
+        sim_bundle.accept_results.join(weights_df, on="simulation", how="left")
+        .with_columns(pl.col("weight").fill_null(0))
+        .with_columns(
+            pl.col("weight")
+            .max()
+            .over(sim_bundle.inputs.drop(["simulation", "randomSeed"]).columns)
+            .alias("weight")
+        )
+    )
+
+
+def marginal_posterior_plot(
+    sim_bundle: SimulationBundle,
+    fig_path: str,
+    log_scale: bool = True,
+):
+    n_parameters = len(sim_bundle.experiment_params)
+    paired_fig, paired_axes = plt.subplots(
+        nrows=n_parameters,
+        ncols=n_parameters,
+        figsize=(5 * n_parameters, 5 * n_parameters),
+        sharex="col",
+    )
+
+    bundle_data = sim_bundle.accept_results
+
+    filename = "marginal_pairs.jpg"
+    file_out = os.path.join(fig_path, filename)
+
+    for row_idx, experiment_param in enumerate(sim_bundle.experiment_params):
+        diag_ax = paired_axes[row_idx, row_idx]
+        diag_ax.hist(bundle_data[experiment_param])
+        for col_idx, comp_param in enumerate(sim_bundle.experiment_params):
+            if col_idx < row_idx:
+                ax_lwr = paired_axes[row_idx, col_idx]
+                ax_lwr.scatter(
+                    bundle_data[comp_param],
+                    bundle_data[experiment_param],
+                    alpha=0.05,
+                )
+                ax_lwr.set_xlabel(comp_param)
+                ax_lwr.set_ylabel(experiment_param)
+
+                if log_scale:
+                    ax_lwr.set_xscale("log")
+                    ax_lwr.set_yscale("log")
+
+                ax_upr = paired_axes[col_idx, row_idx]
+                ax_upr.scatter(
+                    bundle_data[experiment_param],
+                    bundle_data[comp_param],
+                    alpha=0.05,
+                )
+                ax_upr.set_xlabel(experiment_param)
+                ax_upr.set_ylabel(comp_param)
+
+                if log_scale:
+                    ax_upr.set_xscale("log")
+                    ax_upr.set_yscale("log")
+
+        diag_ax.set_xlabel(experiment_param)
+        diag_ax.set_ylabel("Frequency")
+
+    for row in range(n_parameters):
+        for col in range(n_parameters):
+            if row != col:
+                if row != 0:
+                    paired_axes[row, col].sharey(paired_axes[row, 0])
+                else:
+                    paired_axes[row, col].sharey(paired_axes[row, 1])
+
+    paired_fig.savefig(file_out)
+    plt.close(paired_fig)
+
+
+def target_comparison_plot(
+    sim_bundle: SimulationBundle,
+    target_data: pl.DataFrame,
+    fig_path: str,
+    x_col="day",
+    y_col="cumulative_cases",
+    alpha_by_weight=True,
+):
+    duplicate_weights_dataframe(sim_bundle)
+    input_weights = sim_bundle.accept_results.filter(
+        pl.col("simulation").is_in(sim_bundle.results.keys())
+    )
+
+    data_list = []
+    for sim_id, df in sim_bundle.results.items():
+        result = df.filter(
+            pl.col("day") <= target_data["day"].max()
+        ).with_columns(pl.lit(sim_id).alias("simulation"))
+        result = result.join(input_weights, on="simulation", how="left")
+        data_list.append(result)
+
+    # Label axes
+    xlabel = "Day"
+    ylabel = "Cumulative Ascertained Cases"
+
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+
+    for df in data_list:
+        if alpha_by_weight:
+            plt.plot(df[x_col], df[y_col], color="blue", alpha=df["weight"][0])
+        else:
+            plt.plot(
+                df[x_col],
+                df[y_col],
+                color="blue",
+                alpha=0.05,
+            )
+
+    plt.scatter(
+        target_data[x_col],
+        target_data[y_col],
+        color="red",
+        label="Target",
+        zorder=len(data_list) + 1,
+    )
+
+    # Assign figure to object and close plot
+    fig_simulations = plt.gcf()
+    plt.close()
+
+    file_out = os.path.join(
+        fig_path,
+        f"cumulative_timeseries_{sim_bundle.step_number}.jpg",
+    )
+    fig_simulations.savefig(file_out)
 
 
 def plot_xy_data(
