af479344789614c347e13b3cd4c6467af204f660
KOVALW <AD71@cdc.gov>
2025-07-22 20:57:38 +0000

    precommit

--- abctools/tests/test_simulation_bundle.py
+++ abctools/tests/test_simulation_bundle.py
@@ -1,29 +1,31 @@
 import polars as pl
+
 from abctools.abc_classes import SimulationBundle
 
+
 def test_accept_stochastic():
-    inputs = pl.DataFrame({
-        "simulation": range(10),
-        "seed": [0] * 10
-    }).with_columns(
-        (pl.col("simulation") * 0.5).floor().alias("parameter1")
-    )
+    inputs = pl.DataFrame(
+        {"simulation": range(10), "seed": [0] * 10}
+    ).with_columns((pl.col("simulation") * 0.5).floor().alias("parameter1"))
 
     bundle = SimulationBundle(
         inputs=inputs,
         step_number=0,
         baseline_params={},
-        seed_variable_name="seed"
+        seed_variable_name="seed",
     )
 
-    bundle.distances = pl.DataFrame({
-        "simulation": range(10),
-    }).with_columns(
-        (pl.col("simulation") * 0.1).alias("distance")
-    )
+    bundle.distances = pl.DataFrame(
+        {
+            "simulation": range(10),
+        }
+    ).with_columns((pl.col("simulation") * 0.1).alias("distance"))
     bundle.accept_stochastic(tolerance=0.4)
     accepted_totals = bundle.accepted.select(
         pl.sum("acceptance_weight", "accept_bool")
     )
 
-    assert accepted_totals["acceptance_weight"].item() == accepted_totals["accept_bool"].item()
+    assert (
+        accepted_totals["acceptance_weight"].item()
+        == accepted_totals["accept_bool"].item()
+    )
